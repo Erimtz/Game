5d08837a6d29e11cfb176859408004546958d274
Erimtz <erimtz2@gmail.com>
2023-07-21 18:31:06 -0600

    update docker

--- Game/gamesLibrary/src/main/java/com/dh/controller/GamesLibraryController.java
+++ Game/gamesLibrary/src/main/java/com/dh/controller/GamesLibraryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/GamesLibrary")
+@RequestMapping("/gamesLibrary")
 public class GamesLibraryController {
 
     private GamesLibraryService gamesLibraryService;
